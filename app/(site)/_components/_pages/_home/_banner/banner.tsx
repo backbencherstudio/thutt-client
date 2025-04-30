0c0c4c092ea9fbfe66efa21a178e0624a10cd98b
selim121 <selimkhan.bdkhan@gmail.com>
2025-04-30 11:41:14 +0600

    home page banner added

---
+++ app/(site)/_components/_pages/_home/_banner/banner.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from "react";
+import Button from "../../../_reusable/_button/button";
+
+export default function Banner() {
+  return (
+    <div
+      className="bg-cover bg-center bg-no-repeat relative"
+      style={{
+        backgroundImage: "url('/assets/site-images/home/banner.png')",
+      }}
+    >
+      <div className="container mx-auto py-80">
+        <div className="max-w-[624px] flex flex-col gap-4 items-start">
+          <h1 className="text-white">Create Workspaces That Work for You</h1>
+          <p className="text-white text-[20px] font-medium mb-8">
+            Explore new & pre-owned office cubicles and furniture solutions
+          </p>
+          <Button text="Browse Cubicles" href="/" />
+        </div>
+      </div>
+    </div>
+  );
+}
