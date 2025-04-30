716c89ef5665536a14a651c92e604654b6688712
selim121 <selimkhan.bdkhan@gmail.com>
2025-04-30 11:41:14 +0600

    home page banner added

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,9 @@
+import Banner from "./_components/_pages/_home/_banner/banner";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1>Hello World</h1>
+    <div>
+      <Banner />
     </div>
   );
 }
