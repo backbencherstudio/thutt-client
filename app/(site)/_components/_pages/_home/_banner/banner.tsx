f89a55205d1a1e12137616434cc9918b807c58d9
selim121 <selimkhan.bdkhan@gmail.com>
2025-05-03 15:31:39 +0600

    banner and some issues fixed

--- app/(site)/_components/_pages/_home/_banner/banner.tsx
+++ app/(site)/_components/_pages/_home/_banner/banner.tsx
@@ -5,12 +5,12 @@ import Button from "../../../_reusable/_button/button";
 export default function Banner() {
   return (
     <div
-      className="bg-cover bg-center bg-no-repeat relative"
+      className="bg-cover bg-center bg-no-repeat relative px-4"
       style={{
         backgroundImage: "url('/assets/site-images/home/banner.png')",
       }}
     >
-      <div className="container mx-auto py-28 md:py-80 px-4 md:px-0">
+      <div className="container mx-auto py-28 md:py-80 md:px-0">
         <div className="max-w-[624px] flex flex-col gap-4 items-start">
           <h1 className="text-white">Create Workspaces That Work for You</h1>
           <p className="text-white text-[20px] font-medium mb-8">
