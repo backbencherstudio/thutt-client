d6f6c2bf41924d8869bdd8883e95d39e7f981b26
selim121 <selimkhan.bdkhan@gmail.com>
2025-05-03 11:57:34 +0600

    footer added

--- app/(site)/_components/_pages/_home/_banner/banner.tsx
+++ app/(site)/_components/_pages/_home/_banner/banner.tsx
@@ -10,7 +10,7 @@ export default function Banner() {
         backgroundImage: "url('/assets/site-images/home/banner.png')",
       }}
     >
-      <div className="container mx-auto py-80">
+      <div className="container mx-auto py-28 md:py-80 px-4 md:px-0">
         <div className="max-w-[624px] flex flex-col gap-4 items-start">
           <h1 className="text-white">Create Workspaces That Work for You</h1>
           <p className="text-white text-[20px] font-medium mb-8">
