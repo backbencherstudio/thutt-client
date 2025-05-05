52489400ef9f9508e2198625e1b61b9d33afd3f9
selim121 <selimkhan.bdkhan@gmail.com>
2025-05-05 14:18:13 +0600

    category one added

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,12 @@
 import Banner from "./_components/_pages/_home/_banner/banner";
 import About from "./_components/_pages/_home/_about/about";
+import Category from "./_components/_pages/_home/_category/category";
 export default function Home() {
   return (
     <div>
       <Banner />
       <About />
+      <Category />
     </div>
   );
 }
