35186b5220aac2adf3d040d8bdb23ff285233d30
selim121 <selimkhan.bdkhan@gmail.com>
2025-05-06 10:23:22 +0600

    cta has been completed

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,6 +3,7 @@ import About from "./_components/_pages/_home/_about/about";
 import Category from "./_components/_pages/_home/_category/category";
 import Service from "./_components/_pages/_home/_service/service";
 import Portfolio from "./_components/_pages/_home/_portfolio/portfolio";
+import CTA from "./_components/_reusable/_cta/cta";
 export default function Home() {
   return (
     <div>
@@ -11,6 +12,7 @@ export default function Home() {
       <Category />
       <Service />
       <Portfolio />
+      <CTA />
     </div>
   );
 }
