b755cc4ac2585dfab26e0b2ac798ed3c60cad0ed
selim121 <selimkhan.bdkhan@gmail.com>
2025-05-06 12:30:39 +0600

    new cubicles page added with banner and common components

---
+++ app/(site)/category/new-cubicles/page.tsx
@@ -0,0 +1,12 @@
+import Testimonials from "../../_components/_reusable/_testimonials/testimonials";
+import CTA from "../../_components/_reusable/_cta/cta";
+import Banner from "../../_components/_pages/_new-cubicles/_banner/banner";
+export default function Home() {
+  return (
+    <div>
+      <Banner />
+      <CTA />
+      <Testimonials />
+    </div>
+  );
+}
