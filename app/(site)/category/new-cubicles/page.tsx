6041abf02270d988925905288d113917aaf0475a
selim121 <selimkhan.bdkhan@gmail.com>
2025-05-06 16:24:14 +0600

    new product show done

--- app/(site)/category/new-cubicles/page.tsx
+++ app/(site)/category/new-cubicles/page.tsx
@@ -2,11 +2,13 @@ import Testimonials from "../../_components/_reusable/_testimonials/testimonials
 import CTA from "../../_components/_reusable/_cta/cta";
 import Banner from "../../_components/_pages/_new-cubicles/_banner/banner";
 import Feature from "../../_components/_pages/_new-cubicles/_feature/feature";
+import NewCubicles from "../../_components/_pages/_new-cubicles/_cubicles/newCubicles";
 export default function Home() {
   return (
     <div>
       <Banner />
       <Feature />
+      <NewCubicles />
       <CTA />
       <Testimonials />
     </div>
