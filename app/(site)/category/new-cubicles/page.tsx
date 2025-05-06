522f4952b041bb67454858773182199f555d1eff
selim121 <selimkhan.bdkhan@gmail.com>
2025-05-06 15:27:17 +0600

    features section added

--- app/(site)/category/new-cubicles/page.tsx
+++ app/(site)/category/new-cubicles/page.tsx
@@ -1,10 +1,12 @@
 import Testimonials from "../../_components/_reusable/_testimonials/testimonials";
 import CTA from "../../_components/_reusable/_cta/cta";
 import Banner from "../../_components/_pages/_new-cubicles/_banner/banner";
+import Feature from "../../_components/_pages/_new-cubicles/_feature/feature";
 export default function Home() {
   return (
     <div>
       <Banner />
+      <Feature />
       <CTA />
       <Testimonials />
     </div>
