415f0d3cbeeb3de15cd86b1787179a62066f0709
selim121 <selimkhan.bdkhan@gmail.com>
2025-05-07 12:48:32 +0600

    preowned page completed

--- app/(site)/category/pre-owned-cubicles/page.tsx
+++ app/(site)/category/pre-owned-cubicles/page.tsx
@@ -1,10 +1,9 @@
 import Testimonials from "../../_components/_reusable/_testimonials/testimonials";
 import CTA from "../../_components/_reusable/_cta/cta";
-import NewCubicles from "../../_components/_pages/_new-cubicles/_cubicles/newCubicles";
-import WhyChooseUs from "../../_components/_pages/_new-cubicles/_why-choose-us/whyChooseUs";
 import Banner from "../../_components/_pages/_preowned-cubicles/_banner/banner";
 import Feature from "../../_components/_pages/_preowned-cubicles/_feature/feature";
 import PreownedCubicles from "../../_components/_pages/_preowned-cubicles/_cubicles/preownedCubicles";
+import WhyChooseUs from "../../_components/_pages/_preowned-cubicles/_why-choose-us/whyChooseUs";
 export default function Home() {
   return (
     <div>
