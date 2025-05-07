e3e013ad5f5a36f5a1349cb852e15155ec721bcf
selim121 <selimkhan.bdkhan@gmail.com>
2025-05-07 12:39:45 +0600

    some section added for preowned product page

---
+++ app/(site)/_components/_pages/_preowned-cubicles/_banner/banner.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Banner() {
+  return (
+    <div
+      className="bg-cover bg-center bg-no-repeat relative px-4"
+      style={{
+        backgroundImage: "url('/assets/site-images/preowned/banner.png')",
+      }}
+    >
+      <div className="container mx-auto pt-20 pb-[132px]">
+        <div className="max-w-[754px] flex flex-col gap-4 items-start">
+          <div className="flex gap-2 mb-4">
+            <div className="text-[#BDC3FF] text-[20px] md:text-[24px]">
+              Home/
+            </div>
+            <div className="text-[#BDC3FF] text-[20px] md:text-[24px]">
+              Category/
+            </div>
+            <div className="text-white text-[20px] md:text-[24px]">
+              New Cubicles
+            </div>
+          </div>
+          <h1 className="text-white">
+            Smart Cubicles Tailored for Modern Teams
+          </h1>
+          <p className="text-white text-[20px] font-medium mb-8">
+            Explore modular, ergonomic solutions built for collaboration,
+            privacy, and peak productivity.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/(site)/category/pre-owned-cubicles/page.tsx
+++ app/(site)/category/pre-owned-cubicles/page.tsx
@@ -1,15 +1,16 @@
 import Testimonials from "../../_components/_reusable/_testimonials/testimonials";
 import CTA from "../../_components/_reusable/_cta/cta";
-import Banner from "../../_components/_pages/_new-cubicles/_banner/banner";
-import Feature from "../../_components/_pages/_new-cubicles/_feature/feature";
 import NewCubicles from "../../_components/_pages/_new-cubicles/_cubicles/newCubicles";
 import WhyChooseUs from "../../_components/_pages/_new-cubicles/_why-choose-us/whyChooseUs";
+import Banner from "../../_components/_pages/_preowned-cubicles/_banner/banner";
+import Feature from "../../_components/_pages/_preowned-cubicles/_feature/feature";
+import PreownedCubicles from "../../_components/_pages/_preowned-cubicles/_cubicles/preownedCubicles";
 export default function Home() {
   return (
     <div>
       <Banner />
       <Feature />
-      <NewCubicles />
+      <PreownedCubicles />
       <WhyChooseUs />
       <CTA />
       <Testimonials />
