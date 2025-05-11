ef886f0358801b406f8e35ff29d0b464a32f2daf
selim121 <selimkhan.bdkhan@gmail.com>
2025-05-11 10:51:45 +0600

    portfolio card two added

--- app/(site)/portfolio/page.tsx
+++ app/(site)/portfolio/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "../_components/_pages/_portfolio/_banner/banner";
 import CardsOne from "../_components/_pages/_portfolio/_project-cards/_card-one/cardOne";
+import CardTwo from "../_components/_pages/_portfolio/_project-cards/_card-two/cardTwo";
 export default function Services() {
   return (
     <div>
       <Banner />
       <div className="container mx-auto pt-10 sm:pt-16 md:pt-20 xl:pt-[120px]">
         <CardsOne />
+        <CardTwo />
       </div>
     </div>
   );
