ebb8c01831f85856210fd6fba9b5bddae9fea84a
selim121 <selimkhan.bdkhan@gmail.com>
2025-05-11 11:34:31 +0600

    portfolio card three added

--- app/(site)/portfolio/page.tsx
+++ app/(site)/portfolio/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "../_components/_pages/_portfolio/_banner/banner";
 import CardsOne from "../_components/_pages/_portfolio/_project-cards/_card-one/cardOne";
 import CardTwo from "../_components/_pages/_portfolio/_project-cards/_card-two/cardTwo";
+import CardThree from "../_components/_pages/_portfolio/_project-cards/_card-three/cardThree";
 export default function Services() {
   return (
     <div>
@@ -8,6 +9,7 @@ export default function Services() {
       <div className="container mx-auto pt-10 sm:pt-16 md:pt-20 xl:pt-[120px]">
         <CardsOne />
         <CardTwo />
+        <CardThree />
       </div>
     </div>
   );
