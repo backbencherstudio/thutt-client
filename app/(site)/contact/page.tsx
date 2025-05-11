656fb2c82fa23ad386eb032304f06727a5321170
selim121 <selimkhan.bdkhan@gmail.com>
2025-05-11 16:08:06 +0600

    contact form added

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -1,8 +1,10 @@
 import Banner from "../_components/_pages/_contact/_banner/banner";
+import ContactForm from "../_components/_pages/_contact/_contact-form/contactForm";
 export default function Services() {
   return (
     <div>
       <Banner />
+      <ContactForm />
     </div>
   );
 }
