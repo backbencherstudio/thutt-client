ee0e38697b2bd12ab56ce848e7736ced24b8ccbb
selim121 <selimkhan.bdkhan@gmail.com>
2025-05-11 09:46:48 +0600

    portfolio banner added

---
+++ app/(site)/_components/_pages/_portfolio/_banner/banner.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Banner() {
+  return (
+    <div
+      className="bg-cover bg-center bg-no-repeat relative px-4"
+      style={{
+        backgroundImage: "url('/assets/site-images/portfolio/banner.png')",
+      }}
+    >
+      <div className="container mx-auto pt-20 pb-[132px]">
+        <div className="max-w-[754px] flex flex-col gap-4 items-start">
+          <div className="flex gap-2 mb-4">
+            <div className="text-[#BDC3FF] text-[20px] md:text-[24px]">
+              Home/
+            </div>
+            <div className="text-[#BDC3FF] text-[20px] md:text-[24px]">
+              Portfolio
+            </div>
+          </div>
+          <h1 className="text-white">
+            Designed with Purpose. Built with Precision.
+          </h1>
+          <div className="text-white text-[20px] md:text-[24px] font-medium mb-8 leading-[120%]">
+            Explore our curated collection of residential, commercial, and
+            bespoke furniture projects crafted to elevate living.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/(site)/portfolio/page.tsx
@@ -0,0 +1,8 @@
+import Banner from "../_components/_pages/_portfolio/_banner/banner";
+export default function Services() {
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+}
